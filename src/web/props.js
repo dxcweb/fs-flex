978068147f455be701f6f43895c83c1535595d59
郭威 <605863278@qq.com>
2017-10-26 15:12:52 +0800

    no message

--- src/web/props.js
+++ src/web/props.js
@@ -2,11 +2,9 @@
  * Created by dxc on 2016/9/7.
  */
 import abbr from './abbr'
-import unit from './unit'
 export default function (props) {
     El(props);
     abbr(props);
-    unit(props);
 }
 function El(props) {
     if (props.el == null)
